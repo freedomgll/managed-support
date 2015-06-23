ffcd212ba57afd1bc03e8d47c2210c1bfa082bd5
freedomgll <freedomgll@163.com>
2015-06-23 15:55:24 +0800

    Improved linux/hg.sh

--- linux/hg.sh
+++ linux/hg.sh
@@ -2,18 +2,18 @@
 
 #hg pull from  repo of directories
 function pull() {
-	path=.
-	echo $path
 	if [ "$1" != "" ]; then
 		path=$1
-	fi
-	
-	for file in $path/*
-	do 
-		if [ -d "$file" ]
-		then 
-		  echo $file
-		  hg --repository $file pull -u -r default --insecure --verbose
-		fi
-	done
+		for file in $path/*
+		do 
+			if [ -d "$file" ]
+			then 
+			  echo hg --repository $file pull -u -r default --insecure --verbose
+			  hg --repository $file pull -u -r default --insecure --verbose
+			fi
+		done
+	else
+	    echo hg pull -u -r default --insecure --verbose
+		hg pull -u -r default --insecure --verbose
+	fi	
 }
